2f9497ede348bdb46aa2f8d0cfc4c641df1564f4
henrisaing <henri.saing@gmail.com>
2016-09-03 14:55:24 -0400

    Adjusts branding. Adds highlighting

---
+++ public/assets/js/main.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+
+  startTime();
+  updateTimes();
+
+  var currentTime = $('.clock').html();
+
+
+  $('button').click(function(){
+
+    var clicked = $(this).attr('id');
+    var ct = ".ct"+ clicked;
+    var st = ".st"+ clicked;
+    var total = ".total"+clicked;
+    var row = ".row"+clicked;
+
+    if($(st).html() == "Inactive"){
+      $(st).html(getCurrentTime());
+      $(this).html("Reset");
+      $(row).addClass('highlight');
+    }else{
+      $(this).html("Start");
+      $(st).html("Inactive");
+      $(ct).html("0:0:0");
+      $(total).html("$0.00");
+      $(row).removeClass('highlight');
+    }
+
+  });
+
+
+});
+
+function updateTimes(){
+  for(var i = 1; i < 12; i++){
+    var startTimeCheck = $(".st"+i).html();
+    if(startTimeCheck != "Inactive"){
+      var startT = new Date(startTimeCheck);
+      var timeNow = getCurrentTime();
+      var timeDiff = timeNow - startT;
+      var rate = $(".rate"+i).html();
+      $(".ct"+i).html(convertMStoTime(timeDiff));
+      $(".total"+i).html("$"+((timeDiff * rate/ 3600)/1000).toFixed(2));
+    }
+
+  }
+  var t = setTimeout(updateTimes, 1000);
+}
+
+function convertMStoTime(ms){
+  var x;
+  x = ms / 1000;
+  var counter = x ;
+  var secondsTimer = 0;
+  var minutesTimer = 0;
+  var hoursTimer = 0;
+
+  var totalSec = x % 3600
+
+  var hours = parseInt( totalSec / 3600 ) % 24;
+  var minutes = parseInt( totalSec / 60 ) % 60;
+  var seconds = totalSec % 60;
+
+  var time = hours+":"+minutes+":"+seconds.toFixed(0);
+
+
+  return time;
+}
+
+function getCurrentTime(){
+  var today = new Date();
+  return today;
+}
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    // $('div.clock').html(h + ":" + m + ":" + s);
+    $('div.clock').html(today);
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
---
+++ resources/views/tables.blade.php
@@ -0,0 +1,169 @@
+<!-- resources/views/tables.blade.php -->
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                  Time: <div class="clock"></div>
+                </div>
+            </div>
+
+                <div class="panel-body">
+                    <table class="table table-striped tables-table">
+
+                      <!-- table headings -->
+                      <thead>
+                        <th>Table Number</th>
+                        <th>People</th>
+                        <th>Rate</th>
+                        <th>Start / Stop</th>
+                        <th>Start Time</th>
+                        <th>Current Time</th>
+                        <th>Total</th>
+                      </thead>
+
+                      <!-- table body -->
+                      <tbody>
+
+                        <?php for($i = 1; $i < 12; $i++): ?>
+                          <tr class="row<?php print $i ;?>">
+                            <td><?php print $i ;?></td>
+                            <td>1</td>
+                            <td class="rate<?php print $i ;?>">8</td>
+                            <td><button type="button" id="<?php print $i ;?> ">Start</button></td>
+                            <td class="st<?php print $i ;?>">Inactive</td>
+                            <td class="ct<?php print $i ;?>">0:0:0</td>
+                            <td class="total<?php print $i ;?>">$0.00</td>
+                          </tr>
+                        <?php endfor; ?>
+                        <!-- table 1 -->
+                        <!-- <tr>
+                          <td>1</td>
+                          <td>1</td>
+                          <td>8</td>
+                          <td><button type="button" name="button1" id="button1">Start Table</button></td>
+                          <td class="ct1">Time Holder</td>
+                          <td class="total1">Total Holder</td>
+                        </tr>
+
+                        <tr>
+                          <td>2</td>
+                          <td>1</td>
+                          <td>8</td>
+                          <td><button type="button" name="button1" id="button2">Start Table</button></td>
+                          <td>Time Holder</td>
+                          <td class="total1">Total Holder</td>
+                        </tr>
+
+                        <tr>
+                          <td>3</td>
+                          <td>1</td>
+                          <td>8</td>
+                          <td><button type="button" name="button1" id="button3">Start Table</button></td>
+                          <td>Time Holder</td>
+                          <td class="total1">Total Holder</td>
+                        </tr>
+
+                        <tr>
+                          <td>4</td>
+                          <td>1</td>
+                          <td>8</td>
+                          <td><button type="button" name="button1" id="button4">Start Table</button></td>
+                          <td>Time Holder</td>
+                          <td class="total1">Total Holder</td>
+                        </tr>
+
+                        <tr>
+                          <td>5</td>
+                          <td>1</td>
+                          <td>8</td>
+                          <td><button type="button" name="button1" id="button5">Start Table</button></td>
+                          <td>Time Holder</td>
+                          <td>Total Holder</td>
+                        </tr>
+
+                        <tr>
+                          <td>6</td>
+                          <td>1</td>
+                          <td>8</td>
+                          <td><button type="button" name="button1" id="button6">Start Table</button></td>
+                          <td>Time Holder</td>
+                          <td>Total Holder</td>
+                        </tr>
+
+                        <tr>
+                          <td>7</td>
+                          <td>1</td>
+                          <td>8</td>
+                          <td><button type="button" name="button1" id="button7">Start Table</button></td>
+                          <td>Time Holder</td>
+                          <td>Total Holder</td>
+                        </tr>
+
+                        <tr>
+                          <td>8</td>
+                          <td>1</td>
+                          <td>8</td>
+                          <td><button type="button" name="button1" id="button8">Start Table</button></td>
+                          <td>Time Holder</td>
+                          <td>Total Holder</td>
+                        </tr>
+
+                        <tr>
+                          <td>9</td>
+                          <td>1</td>
+                          <td>8</td>
+                          <td><button type="button" name="button1" id="button9">Start Table</button></td>
+                          <td>Time Holder</td>
+                          <td>Total Holder</td>
+                        </tr>
+
+                        <tr>
+                          <td>10</td>
+                          <td>1</td>
+                          <td>8</td>
+                          <td><button type="button" name="button1" id="button10">Start Table</button></td>
+                          <td>Time Holder</td>
+                          <td>Total Holder</td>
+                        </tr>
+
+                        <tr>
+                          <td>11</td>
+                          <td>1</td>
+                          <td>8</td>
+                          <td><button type="button" name="button1" id="button11">Start Table</button></td>
+                          <td>Time Holder</td>
+                          <td>Total Holder</td>
+                        </tr> -->
+                      </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+<!-- <div class="panel panel-default">
+                    <div class="panel-heading">
+                      Current Inventory
+                    </div>
+
+                    <div class="panel-body">
+                      <table class="table table-striped item-table">
+
+                        <! table headings -->
+                        <!-- <thead>
+                          <th>Item</th>
+                          <th>Stock</th>
+                          <th>Last Updated</th>
+                        </thead> -->
+
+                        <!-- table body -->
+                        <!-- <tbody> -->
+                            <!-- <tr> -->
+                              <!-- item name -->
+                              <!-- <td> -->
