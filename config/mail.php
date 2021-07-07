91fa616d1c3b7a16d324e168ad95950b08dfc053
S18305452\Administrator <henri.saing@gmail.com>
2021-07-07 09:05:05 -0500

    Updates from=> array in mail.php.

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'uisnotc.noreply@gmail.com', 'name' => 'RateTimer'],
 
     /*
     |--------------------------------------------------------------------------
